b77aa3a47449126c56a50f35112b3090c47e1d8d
Brennan_Webb <brennan.webb@selectquote.com>
2021-02-10 09:58:29 -0500

    upload

---
+++ sp_Columnizer.sql
@@ -0,0 +1,148 @@
+﻿USE [AdHocData]
+GO
+/****** Object:  StoredProcedure [PSLAO].[sp_Columnizer]    Script Date: 1/27/2021 10:22:02 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*
+-----------------------------------------------------------------------------------------------------------------------------------------
+Author: Brennan Webb
+Date Written: 12/18/2020
+Called From:  For Adhoc Use
+
+Purpose: Allows a user to supply a table name to return metadata on the table in different concatenated forms.  Use at least a 3 part naming convention for object references.
+Example Usage Script (just highlight the lines below and execute):
+
+	Exec AdHocData.pslao.sp_Columnizer @version=1,@object='AdhocData.pslao.DPM_CASMap';
+	Exec AdHocData.pslao.sp_Columnizer @version=2,@object='AdhocData.pslao.DPM_CASMap';
+	Exec AdHocData.pslao.sp_Columnizer @version=3,@object='AdhocData.pslao.DPM_CASMap';
+	Exec AdHocData.pslao.sp_Columnizer @version=4,@object='AdhocData.pslao.DPM_CASMap';
+
+@Version=1 can also be used with other tools in the DB such as sp_agger.  Example below.
+
+Declare @Columns Varchar(max);
+Exec AdHocData.pslao.sp_Columnizer @version=1,@object='AdhocData.pslao.DPM_CASMap',@Columns=@Columns Output;
+Exec AdHocData.pslao.sp_Agger 'Sum',@Columns;
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+Modification History
+-----------------------------------------------------------------------------------------------------------------------------------------
+Revision      Author        Date                 Reason
+------------- ---------     -------------------- -------------------------------------------------------------------------------------
+00000         BBW			12/18/2020           Implemented
+________________________________________________________________________________________________________________
+*/
+CREATE proc [PSLAO].[sp_Columnizer] (@version int, @object varchar(200),@Columns varchar(max)=Null Output)
+as
+BEGIN
+	If @version = 1 goto one; --@version=1 will output column headers only in a single line comma separated string.
+	If @version = 2 goto two; --@version=2 will output column headers + dataType with lengths in a single line comma separated string. Helpful for create table statements.
+	If @version = 3 goto three; --@version=3 will output column headers only in vertical position.
+	If @version = 4 goto four; --@version=4 will output column headers + dataType with lengths in vertical position.
+	return;
+END
+
+one:
+	Begin
+		If @object like '#%' 
+			Begin
+				SELECT @Columns = COALESCE(@Columns + ', ','') + QUOTENAME(COLUMN_NAME)
+				FROM TEMPDB.INFORMATION_SCHEMA.COLUMNS C
+				Where TABLE_NAME like parsename(@object,1)+'%'
+				ORDER BY ORDINAL_POSITION;
+				Select @Columns as Columns_to_String_1;
+				Return;
+			End
+		Else
+		SELECT @Columns = COALESCE(@Columns + ', ','') + QUOTENAME(COLUMN_NAME)
+		FROM AdhocData.INFORMATION_SCHEMA.COLUMNS C
+		Where TABLE_CATALOG = parsename(@object,3)
+		and TABLE_SCHEMA = parsename(@object,2)
+		and TABLE_NAME = parsename(@object,1)
+		ORDER BY ORDINAL_POSITION;
+		Select @Columns as Columns_to_String_1;
+		Return;
+	End;
+
+two:
+	Begin
+		If @object like '#%' 
+			Begin
+				SELECT @Columns = COALESCE(@Columns + ', ','') + QUOTENAME(COLUMN_NAME) +' '+ Case when CHARACTER_MAXIMUM_LENGTH is null then Data_Type
+																						   when CHARACTER_MAXIMUM_LENGTH = -1 then Data_Type +'(MAX)'
+																						   when CHARACTER_MAXIMUM_LENGTH is not null then Data_Type +'('+cast(CHARACTER_MAXIMUM_LENGTH as varchar(20))+')'
+																						   End
+				FROM tempdb.INFORMATION_SCHEMA.COLUMNS C
+				Where TABLE_NAME like parsename(@object,1)+'%'
+				ORDER BY ORDINAL_POSITION;
+				Select @Columns as Columns_to_String_2;
+				Return;
+			End
+		Else
+		SELECT @Columns = COALESCE(@Columns + ', ','') + QUOTENAME(COLUMN_NAME) +' '+ Case when CHARACTER_MAXIMUM_LENGTH is null then Data_Type
+																						   when CHARACTER_MAXIMUM_LENGTH = -1 then Data_Type +'(MAX)'
+																						   when CHARACTER_MAXIMUM_LENGTH is not null then Data_Type +'('+cast(CHARACTER_MAXIMUM_LENGTH as varchar(20))+')'
+																						   End
+		FROM AdhocData.INFORMATION_SCHEMA.COLUMNS C
+		Where TABLE_CATALOG = parsename(@object,3)
+		and TABLE_SCHEMA = parsename(@object,2)
+		and TABLE_NAME = parsename(@object,1)
+		ORDER BY ORDINAL_POSITION;
+		Select @Columns as Columns_to_String_2;
+		Return;
+	End;
+
+three:
+	Begin
+		If @object like '#%' 
+			Begin
+				SELECT Case when ORDINAL_POSITION=1 then '' else ',' end+
+				' '+QUOTENAME(COLUMN_NAME) Columns_Vertical_3
+				FROM Tempdb.INFORMATION_SCHEMA.COLUMNS C
+				Where TABLE_NAME like parsename(@object,1)+'%'
+				ORDER BY ORDINAL_POSITION;
+				Return;
+			End
+		Else
+		SELECT Case when ORDINAL_POSITION=1 then '' else ',' end+
+		' '+QUOTENAME(COLUMN_NAME) Columns_Vertical_3
+		FROM AdhocData.INFORMATION_SCHEMA.COLUMNS C
+		Where TABLE_CATALOG = parsename(@object,3)
+		and TABLE_SCHEMA = parsename(@object,2)
+		and TABLE_NAME = parsename(@object,1)
+		ORDER BY ORDINAL_POSITION;
+		Return;
+	End;
+
+four:
+	Begin
+		If @object like '#%' 
+			Begin
+				SELECT Case when ORDINAL_POSITION=1 then '' else ',' end+
+				' '+QUOTENAME(COLUMN_NAME) +
+				' '+ Case when CHARACTER_MAXIMUM_LENGTH is null then Data_Type
+				when CHARACTER_MAXIMUM_LENGTH = -1 then Data_Type +'(MAX)'
+				when CHARACTER_MAXIMUM_LENGTH is not null then Data_Type +'('+cast(CHARACTER_MAXIMUM_LENGTH as varchar(20))+')'
+				End Columns_Vertical_4
+				FROM Tempdb.INFORMATION_SCHEMA.COLUMNS C
+				Where TABLE_NAME like parsename(@object,1)+'%'
+				ORDER BY ORDINAL_POSITION;
+				Return;
+			End
+		Else
+		SELECT Case when ORDINAL_POSITION=1 then '' else ',' end+
+		' '+QUOTENAME(COLUMN_NAME) +
+		' '+ Case when CHARACTER_MAXIMUM_LENGTH is null then Data_Type
+		when CHARACTER_MAXIMUM_LENGTH = -1 then Data_Type +'(MAX)'
+		when CHARACTER_MAXIMUM_LENGTH is not null then Data_Type +'('+cast(CHARACTER_MAXIMUM_LENGTH as varchar(20))+')'
+		End Columns_Vertical_4
+		FROM AdhocData.INFORMATION_SCHEMA.COLUMNS C
+		Where TABLE_CATALOG = parsename(@object,3)
+		and TABLE_SCHEMA = parsename(@object,2)
+		and TABLE_NAME = parsename(@object,1)
+		ORDER BY ORDINAL_POSITION;
+		Return;
+	End;
+GO
---
+++ sp_Search_Module_Definitions.sql
@@ -0,0 +1,73 @@
+USE [SRA]
+GO
+/****** Object:  StoredProcedure [ADMIN].[sp_search_module_definitions]    Script Date: 07/14/2020 12:43:40 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE proc [ADMIN].[sp_search_module_definitions]
+(
+@db varchar(50) = null,
+@searchforstring nvarchar(max)= null,
+@returnfirstfind int = 0
+)
+as
+set nocount on
+declare
+	@sql varchar(max),
+	@i int =1
+	;
+
+	--collect all db's
+	if object_id('tempdb..##db_loop','u') is not null drop table ##db_loop;
+	select name, database_id 
+	into ##db_loop
+	from master.sys.databases
+	where name in (case when @db is null then (Select name from master.sys.databases) else @db end)
+	and database_id>4 and database_id not in (11,12);
+
+	--setup loop table for matching records.
+	if object_id('tempdb..##search_results','u') is not null drop table ##search_results;
+	create table ##search_results ([object_id/job_id] nvarchar(max), [definition/command] nvarchar(max),[name] nvarchar(max),[type_desc/step_name] nvarchar(max));
+
+	--loop through db instances unless explicit instance is provided.
+	while @i<=(select max(database_id) from ##db_loop)
+		begin
+			set @db=(select name from ##db_loop where database_id=@i);
+			if @db is null goto skip1;
+			
+			--search module definitions within in each db.
+			if object_id('tempdb..##sproc_loop','u') is not null drop table ##sproc_loop;
+			set @sql='select cast(m.[object_id] as varchar(500))[object_id]
+			              ,m.[definition]
+						  ,'''+@db+'''+''.''+object_schema_name(m.[object_id],db_id('''+@db+'''))+''.''+ o.[name] [name]
+						  ,cast(o.[type_desc] collate latin1_general_ci_ai as nvarchar(max))[type_desc]
+						  into ##sproc_loop 
+						  from '+@db+'.[sys].[all_sql_modules]  m
+						  left join '+@db+'.[sys].[all_objects] o on m.[object_id]=o.[object_id]
+						  where definition like ''%' + @searchforstring + '%''
+					 ' 
+					  ; 
+			print @sql;
+			exec (@sql);
+
+			insert into ##search_results ([object_id/job_id], [definition/command],[name],[type_desc/step_name])
+			select [object_id],[definition],[name],[type_desc] from ##sproc_loop;
+			
+			skip1:
+			set @i=@i+1
+		end
+
+	select *
+	from ##search_results
+	union all
+	select cast(a.job_id as varchar(500))job_id
+	,a.name
+	,b.step_name
+	,b.command collate latin1_general_ci_ai
+	from [msdb].[dbo].[sysjobs] a
+	left join [msdb].[dbo].[sysjobsteps] b on a.[job_id] = b.[job_id]
+	where b.command like '%' + @searchforstring + '%'
+GO
