7b44943b57d4aca80f84b5854582c9e32244f453
Brennan_Webb <brennan.webb@selectquote.com>
2024-01-26 15:39:36 -0500

    folder changes and corrections to dev tools

---
+++ Scraps/Audit_Database_Event_Trigger.sql
@@ -0,0 +1,53 @@
+Create schema [adt]
+Go
+
+CREATE TABLE  [adt].[Event](
+	Event_ID int identity(1,1) Primary Key,
+	[Event_Data] [xml] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+CREATE OR ALTER view [adt].[v_Event]
+as
+SELECT
+Event_ID,
+[event_data].value('(/EVENT_INSTANCE/EventType)[1]','Varchar(150)')[EventType],
+[event_data].value('(/EVENT_INSTANCE/PostTime)[1]','DATETIME')[Timestamp],
+[event_data].value('(/EVENT_INSTANCE/LoginName)[1]','Varchar(150)')LoginName,
+[event_data].value('(/EVENT_INSTANCE/UserName)[1]','Varchar(150)')UserName,
+[event_data].value('(/EVENT_INSTANCE/DatabaseName)[1]','Varchar(150)')DatabaseName,
+[event_data].value('(/EVENT_INSTANCE/SchemaName)[1]','Varchar(150)')SchemaName,
+[event_data].value('(/EVENT_INSTANCE/ObjectName)[1]','Varchar(500)')ObjectName,
+[event_data].value('(/EVENT_INSTANCE/ObjectType)[1]','Varchar(150)')ObjectType,
+LTRIM([event_data].value('(/EVENT_INSTANCE/TSQLCommand/CommandText)[1]','Varchar(max)'))CommandText,
+[event_data].query('(/EVENT_INSTANCE/Parameters/Param)')[Parameters]
+From adt.[Event];
+
+GO
+
+CREATE or Alter TRIGGER [trg_Event]
+ON Database
+FOR 
+	DDL_DATABASE_LEVEL_EVENTS
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	If SYSTEM_USER<>'SQIS-CORP\svc-prd_sql' --Don't log SQL Agent service account
+	Begin
+		INSERT INTO [adt].[Event] (
+			Event_Data
+		)
+		VALUES (
+			EVENTDATA()
+		);
+	End;
+	
+	--Maintenance adt.event table.Keep only the last 100K events.
+	Declare  @EventLimit bigint = (Select Isnull(Max(Event_ID),0)-100000 From adt.[Event])
+	Delete from adt.[Event] Where Event_ID < @EventLimit ;
+
+END;
+GO
+
