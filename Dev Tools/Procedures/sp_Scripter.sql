2d80439a41bff5064ca5176f8c3f56cbffbcd2d8
Brennan_Webb <brennan.webb@selectquote.com>
2025-03-20 21:39:12 -0400

    Version 1

--- Dev Tools/Procedures/sp_Scripter.sql
+++ Dev Tools/Procedures/sp_Scripter.sql
@@ -4,21 +4,29 @@ GO
 CREATE or ALTER   proc [dbo].[sp_Scripter]
 (
 	@sql NVARCHAR(MAX),
-	@order_by NVARCHAR(MAX) = NULL, 
+	@sort NVARCHAR(MAX) = NULL,
+	@filter NVARCHAR(MAX) = NULL,
 	@replace_matrix NVARCHAR(MAX),
 	@template NVARCHAR(MAX),
-	@print bit = 0,
+	@print BIT = 0,
 	@Debug BIT = 0
 
 )
-as
+AS
 
 SET NOCOUNT ON
 SET STATISTICS XML OFF --Required for use of sp_describe_first_result_set
 SET STATISTICS PROFILE OFF --Required for use of sp_describe_first_result_set
+--------------------------------------------------------------------------------------------------------
+/*Check that all parameters are supplied.*/ 
+IF @sql IS NULL OR @replace_matrix IS NULL OR @template IS NULL GOTO Help;
+--------------------------------------------------------------------------------------------------------
 
 DECLARE @VersionHistory VARCHAR(MAX) ='Version Release Notes: https://github.com/BrennanWebb/pubSQL/blob/main/Dev%20Tools/Procedures/sp_Scripter%20Release%20Notes.txt'
-DECLARE @NewID VARCHAR(8) =LEFT (NEWID(),8); IF @Debug = 1 PRINT'##'+@NewID+CHAR(10);
+DECLARE @NewID VARCHAR(8) =LEFT (NEWID(),8); 
+
+IF @Debug = 1 PRINT'##'+@NewID+CHAR(10);
+IF @Debug = 1 PRINT '@sql: '+@sql+CHAR(10);
 
 --------------------------------------------------------------------------------------------------------
 /*First stage. This section creates a randomized, dynamically created temp table that mirrors the source.
@@ -81,38 +89,54 @@ SET @columns = LEFT(@columns, LEN(@columns) - 1);IF @Debug=1 PRINT @columns+CHAR
 SET @column_names = LEFT(@column_names, LEN(@column_names) - 1); IF @Debug=1 PRINT @column_names+CHAR(10);
 
 -- Final SQL to create a temp table
-DECLARE @tableSQL NVARCHAR(MAX) = 'DROP TABLE IF EXISTS ##'+@NewID+'; '+CHAR(10)+
-								 +'CREATE TABLE ##'+@NewID+' (ID INT IDENTITY(1,1),' + @columns + '); '+CHAR(10);
+DECLARE @tableSQL NVARCHAR(MAX) = 'DROP TABLE IF EXISTS ##'+@NewID+'_Prep; '+CHAR(10)+
+								 +'CREATE TABLE ##'+@NewID+'_Prep (ScripterID INT IDENTITY(1,1),' + @columns + '); '+CHAR(10);
 IF @print=1 PRINT @tableSQL+CHAR(10);
 EXEC sp_executesql @tableSQL;
---------------------------------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------------------------------
 /*Second stage.  Now that we have the randomized, dynamically created temp table from the query/proc metadata, we can now get results into the temp table.
 */
 IF @sql LIKE '%!Select %' ESCAPE'!'
 	BEGIN
-		SET @sql ='Insert Into ##'+@NewID+'('+@column_names+') '+CHAR(10)+
+		SET @sql ='Insert Into ##'+@NewID+'_Prep ('+@column_names+') '+CHAR(10)+
 				  +'Select '+@column_names+' '+CHAR(10)+
 				  +'From ('+@sql+')A'+CHAR(10)+
-				  +IIF(@order_by IS NOT NULL,'Order by '+@order_by,'')
+				  +IIF(@sort IS NOT NULL,'Order by '+@sort,'')
 		IF @print=1 PRINT @sql+CHAR(10);
 		EXEC sp_executesql @sql;
 	END;
 ELSE IF @sql NOT LIKE '%!Select %' ESCAPE'!'
 	BEGIN
-		SET @sql ='Insert Into ##'+@NewID+'('+@column_names+')'+CHAR(10)+
+		SET @sql ='Insert Into ##'+@NewID+'_Prep ('+@column_names+')'+CHAR(10)+
 				  ' EXEC sp_executesql N'''+@sql+''' ';
 		IF @print=1 PRINT @sql+CHAR(10);
 		EXEC sp_executesql @sql;
     END;
 
---------------------------------------------------------------------------------------------------------
+--Clean the data per the @filter.
+IF @Filter IS NOT NULL
+	BEGIN
+		SET @sql ='Select * '+CHAR(10)+
+		+'Into ##'+@NewID+' '+CHAR(10)+
+		+'From ##'+@NewID+'_Prep'+CHAR(10)+
+		+'Where '+@Filter;
+		EXEC sp_executesql @sql;
+	END
+ELSE
+	BEGIN
+		SET @sql ='Select * '+CHAR(10)+
+		+'Into ##'+@NewID+' '+CHAR(10)+
+		+'From ##'+@NewID+'_Prep';
+		EXEC sp_executesql @sql;
+	END
 
+--------------------------------------------------------------------------------------------------------
 /*Third stage.  Now that we have dynamically added all results from our query or proc into a randomized temp table which has an identity,
 we can apply our template text and do replacements.*/
 
 --Splitout the replacement_matrix string
-SET @sql = 'Select Identity(int,1,1) ID '+CHAR(10)+
+SET @sql = 'Select Identity(int,1,1) ScripterID '+CHAR(10)+
 		  +',TRIM(SUBSTRING([Value], 1, CHARINDEX(''='', [Value]) - 1)) [Value] '+CHAR(10)+
 		  +',TRIM(SUBSTRING([Value], CHARINDEX(''='', [Value]) + 1, LEN([Value]))) [Replacement] '+CHAR(10)+ 
 		  +'into ##'+@NewID+'_replace_matrix '+CHAR(10)+
@@ -122,16 +146,17 @@ EXEC sp_executesql @sql;
 
 --preset the proper escapement for the template
 SET @template= REPLACE(@template,'''',''''''); 
+IF @Debug=1 PRINT '@template: '+@template+CHAR(10);
 
 --Apply the Replacement_Matrix to the template.
 SET @sql = '
 Declare @i				int		=  1,
 		@Value			varchar(max),
 		@Replacement	varchar(max);
-While @i<= (Select Max(ID) From ##'+@NewID+'_replace_matrix)
+While @i<= (Select Max(ScripterID) From ##'+@NewID+'_replace_matrix)
 	BEGIN
-		Set @Value =(Select [Value] From ##'+@NewID+'_replace_matrix Where ID=@i);
-		Set @Replacement =(Select [Replacement] From ##'+@NewID+'_replace_matrix Where ID=@i);
+		Set @Value =(Select [Value] From ##'+@NewID+'_replace_matrix Where ScripterID=@i);
+		Set @Replacement =(Select [Replacement] From ##'+@NewID+'_replace_matrix Where ScripterID=@i);
 
 		Select @template = Replace(@Template,@Value,''''''+Cast(''+@Replacement+'' as Varchar(max))+'''''')
 		Set @i=@i+1;
@@ -139,7 +164,7 @@ While @i<= (Select Max(ID) From ##'+@NewID+'_replace_matrix)
 '
 IF @Debug=1 PRINT @sql+CHAR(10);
 EXEC sp_executesql @sql, N'@template NVARCHAR(MAX) Output',@template=@template OUTPUT ; 
-IF @Print=1 PRINT @template+CHAR(10);
+IF @Debug=1 PRINT '@template: '+@template+CHAR(10);
 
 --Cross apply the template.
 SET @sql = 'SELECT '''+@template+'''Script '+CHAR(10)+
@@ -152,124 +177,85 @@ RETURN;
 HELP:
 PRINT '
 /*
-sp_Scripter allows a user to supply a SQL Query or proc, a template, and a replacement matrix for the purpose of dynamic script generation.
+sp_Scripter allows a user to supply any SQL Query or Procedure, a user template, and a replacement matrix for the purpose of dynamic script generation.
+This proc only generates script and will never auto-execute DML or DDL on behalf of the user.
+See below for examples.
 
 GitHub: https://github.com/BrennanWebb/pubSQL/blob/main/Dev%20Tools/Procedures/sp_Scripter.sql
 
 PARAMETERS:
-	@search nvarchar(500)	-- This is the term to be searched.  % can be used mid string wildcard operations. 
-								ex. @search = ''From %account'' will return procs containing the string "From dbo.accounts". 
-								An exclamation point in your search term "!" can be used to handle special characters (such as underscores and brackets) 
-								normally reserved for SQL LIKE operations to be handled as a string literal.
-	@db varchar(128)		-- Specify the database(s) if trying to limit results. Use comma separated string, else the entire server will be searched per the granted user permissions.
-	@type varchar(50)		-- Distinguishes the type of search being performed. See below for short codes and examples of types.
-	@sys_obj bit			-- Suppress system objects by default
-	@sort varchar(1000)		-- Pass in a list of comma separated columns to get a custom sort order for returned data.
-	@print bit				-- Use this parameter to have the proc print the exec''d code.
-	@filter varchar(1000)	-- Post query filter on the returned data set.  This is executed inside of a where clause. Ex. @filter=''[Database]=''''GIS_Household'''''', 
-
-TYPES: 
-	sp_search will also allow searches by type, such as column searches and index searches. There are additional search types as well, some less common in usage.
-	
-	@type IS NULL --General search
-	@type in (''index'',''ix'',''i'')
-	@type in (''column'',''col'',''c'')
-	@type in (''QueryStats'',''qs'')
-	@type in (''replication'',''repl'',''r'')
-	@type in(''Reference'', ''ref'')
-	@type in(''Permission'',''Permissions'',''perm'',''pm'')
-	@type in(''Partition'',''part'',''pd'')
-
-See usage examples below.
-
+	@sql NVARCHAR(MAX)				-- This is the sql query or proc that will determine the @template iterations as well as replacement values.
+	@sort NVARCHAR(MAX)				-- If any sorting on the SQL query above is needed, provide it here.  Only comma separated feilds are needed. See examples below.
+	@filter NVARCHAR(MAX)			-- Allows the user to 
+	@replace_matrix NVARCHAR(MAX)	-- This is the replacement matrix for which a user will supply an equivalence string of their choosing.  Comma separations are required between replacement equivalences.
+	@template NVARCHAR(MAX)			-- The template is what will be reproduced with proper replacements for every record in the original SQL query or procedure provided via @sql.
+	@print BIT						-- Print''s the DML operations performed by sp_Scripter so that users can view sp_scripter operations.
+	@debug BIT						-- Helpful for debugging issues with sp_Scripter
 --------------------------------------------------------------------
-**Note** - Some of these executions require View Definition as well as View Server State permission.
-*/
--------------------------------------------------------------------------------------------------------
---General Object Search
--------------------------------------------------------------------------------------------------------
---Search for any object or sql agent job that has ''Search_term'' in the name.
-sp_search @search =''Search_term'' 
-
---Search for any object (EXCLUDING system object names) or sql agent job that has ''Search_term'' in the name and exist only in the specified DB.
-sp_search @search =''Search_term'', @db =''Database_Name'' 
-
---Search for any object (INCLUDING system object names) or sql agent job that has ''Search_term'' in the name and exist only in the specified DB.
-sp_search @search =''Search_term'', @db =''Database_Name'', @sys_obj = 1 
-
+**Note** - This will not bypass server permissions. */
 
 -------------------------------------------------------------------------------------------------------
---Column Search
+--Using a Query
 -------------------------------------------------------------------------------------------------------
---Search for any column (EXCLUDING system columns) that has ''Search_term'' in the column name and exist only in the specified DB.
-sp_search @search =''Search_term'', @db =''Database_Name'', @type = ''column'' --You can also supply ''col'' or simply ''c'' to denote you want the search type on columns.
+/*This is a bit silly, but it demonstrates how you would take a query (this one is simply using sys.schema table) 
+and apply a template (this particular one is mimicing the creation of job steps) and how the replacement matrix is applied.  
 
---Search for any column (INCLUDING system columns) that has ''Search_term'' in the column name and exist only in the specified DB.
-sp_search @search =''Search_term'', @db =''Database_Name'', @sys_obj = 1 , @type = ''column'' --You can also supply ''col'' or simply ''c'' to denote you want the search type on columns.
+NOTE!!!! Keep in mind that whatever you use as your replacement value (left side of the''='') will be used in a replacement statement.
+Be careful to use a replacement string that is unique to your template and distinguishable (ex. ''{var1}'').*/
 
+DECLARE @sql NVARCHAR(MAX)=''
+	SELECT [name],[schema_id],[principal_id] 
+	FROM sys.schemas
+	WHERE schema_id<10000 
+	and name <>''''dbo''''
+'';
+DECLARE @sort NVARCHAR(MAX) = ''[name]''; 
+DECLARE @replace_matrix VARCHAR(MAX) = ''{Var1}=[ScripterID],{Var2}=[name],{Var3}=[schema_id],{Var4}=[principal_id]''; --[ScripterID] is built into sp_scripter as a default identity column 
+DECLARE @template NVARCHAR(MAX) = ''
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, 
+		@step_name=N''''Select ''''{Var2}'''' '''', 
+		@step_id={Var1}, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N''''TSQL'''', 
+		@command=N''''Select {Var2} [name],{Var3} [schema_id] ,{Var4} [principal_id] '''', 
+		@database_name=N''''Master'''', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+'';
 
--------------------------------------------------------------------------------------------------------
---Index Search
--------------------------------------------------------------------------------------------------------
---Search index metadata for ''Search_term'' in the table name or index name over the entire server.
-sp_search @search =''Search_term'', @type = ''index'' --You can also supply ''ix'' or simply ''i'' to denote you want the search type on indexes.
-
---Search index metadata for ''Search_term'' in the table name or index name and exist only in the specified DB.
-sp_search @search =''Search_term'', @db =''Database_Name'', @type = ''index'' --You can also supply ''ix'' or simply ''i'' to denote you want the search type on indexes.
-
---Index search also allows for fully qualified 3 part comma separated objects (database.schema.table). If a fully qualified object is supplied, this will supercede any @db comma separated strings. 
-sp_search @search = ''msdb.dbo.sysjobs,msdb.dbo.sysjobhistory'' ,@type=''i'';
-
--------------------------------------------------------------------------------------------------------
---Query Stats Search
--------------------------------------------------------------------------------------------------------
---Search sys.dm_exec_query_stats for ''Search_term'' in the query text.  This function can be filtered just to a specific db, as query_stats are server wide.
---@db is not necessary in this usage since query stats are server level; @db will be ignored.
-sp_search @search =''Search_term'', @db =''Database_Name'', @type = ''QueryStats'' --You can also supply ''qs'' to denote you want the search type on query stats.
-
-
--------------------------------------------------------------------------------------------------------
---Replication Search
--------------------------------------------------------------------------------------------------------
---Search replication objects for ''Search_term'' in subscription and publication names and sources.    
-sp_search @search =''Search_term'', @db =''Database_Name'', @type = ''repl'' --You can also supply ''r'' to denote you want the search type on replication objects.
-
-
--------------------------------------------------------------------------------------------------------
---Referenced Entity Search
--------------------------------------------------------------------------------------------------------
---Search referenced objects for ''Search_term'' via sys.dm_sql_referenced_entities.    
-sp_search @search =''Search_term'', @db =''Database_Name'', @type = ''reference'' --You can also supply ''ref'' to denote you want the search type on refrence objects.
-
+EXEC sp_Scripter @sql, @sort ,@replace_matrix ,@template 
 
 -------------------------------------------------------------------------------------------------------
---Permissions Search
+--Using a Procedure
 -------------------------------------------------------------------------------------------------------
---Search permissions for ''Search_term'' via sys.database_permissions and sys.database_roles.
-sp_search @search =''<User>, <Login>, Or <Securable>'', @db =''Database_Name'', @type = ''permission'' 
+/*The example below demonstrates how you would take a proc (this one is using sp_who) 
+and apply a template (this template is a kill demo) and how the replacement matrix is applied.
+This demo also shows how @filter works as well.
 
---If @db is not specified, the entire server will be searched by @search term.
-sp_search @search =''<User>, <Login>, Or <Securable>'', @type = ''permission'';
+NOTE!!!! Keep in mind that whatever you use as your replacement value (left side of the''='') will be used in a replacement statement.
+Be careful to use a replacement string that is unique to your template and distinguishable (ex. ''{var1}'').*/
 
---If @db and @search is not specified, the entire server will be searched by @search term.
-sp_search @type = ''perm''; 
+DECLARE @sql NVARCHAR(MAX)=''sp_who'';
+DECLARE @sort NVARCHAR(MAX) = ''[name]''; 
+DECLARE @filter NVARCHAR(MAX) = ''[Status] like ''''%Sleep%'''' '';
+DECLARE @replace_matrix VARCHAR(MAX) = ''{Var1}=[ScripterID],{Var2}=[Spid],{Var3}=[Status],{Var4}=[LogiName]''; --[ScripterID] is built into sp_scripter as a default column 
+DECLARE @template NVARCHAR(MAX) = ''
+If ''''Sleeping'''' = ''''{Var3}'''' 
+	BEGIN
+		Kill {Var2}; --Don''''t actually exec this!!! It''''s just a demo :)
+	END;
+'';
 
-
--------------------------------------------------------------------------------------------------------
---Partition Search
--------------------------------------------------------------------------------------------------------
---Search partitions for ''Search_term'' via sys.indexes and sys.partitions.
-sp_search @search =''<tableName>, <indexName>, Or <partitionfunctionName>'', @db =''Database_Name'', @type = ''partition'' 
-
---If @db is not specified, the entire server will be searched by @search term.
-sp_search @search =''<tableName>, <indexName>, Or <partitionfunctionName>'', @type = ''partition'';
-
---If @db and @search is not specified, the entire server will be searched by @search term.
-sp_search @type = ''part''; 
+EXEC sp_Scripter @sql, @sort ,@filter,@replace_matrix ,@template
 
 --------------------------------------------------------------------
 
 '
-
-Print '/*'+@VersionHistory+'*/';
-Return;
+PRINT '/*'+@VersionHistory+'*/';
+RETURN;
