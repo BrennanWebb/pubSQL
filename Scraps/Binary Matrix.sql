38e0201545c3ec3423c24cfc3fc377cdb68b0d45
Brennan_Webb <brennan.webb@selectquote.com>
2025-07-01 17:17:43 -0400

    Generic commit message

--- Scraps/Binary Matrix.sql
+++ Scraps/Binary Matrix.sql
@@ -1,15 +1,17 @@
 Drop table if exists #t2
 SELECT n ID,
-CASE WHEN n&1 >0 THEN 1 ELSE 0 END AS a,
-CASE WHEN n&2 >0 THEN 1 ELSE 0 END AS b,
-CASE WHEN n&4 >0 THEN 1 ELSE 0 END AS c,
-CASE WHEN n&8 >0 THEN 1 ELSE 0 END AS d
+IIF(n&1 >0, 1, 0) AS a,
+IIF(n&2 >0, 1, 0) AS b,
+IIF(n&4 >0, 1, 0) AS c,
+IIF(n&8 >0, 1, 0) AS d
+
+--,CASE WHEN n&16 >0 THEN 1 ELSE 0 END AS e --add more columns as needed, however be sure to up the second number in the Power() function below.
 Into #t2
 FROM
 (	SELECT number n
-	FROM master..spt_values
+	FROM master..spt_values --just get some values
 	WHERE TYPE ='p'
-	AND number <Power(2,4) --Change powers as needed.  Use 2 for binary testing (ergo True False).
+	AND number <Power(2,4) --Power(Use 2 for binary testing (ergo True False), second value in the Power() function must match the number of columns) 
 ) Nums
 
 Select * From #T2;
